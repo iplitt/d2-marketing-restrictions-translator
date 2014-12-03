fa26433470686c273e8f6aa59c9c3fa4e56b7e66
iplitt <iplitt@alum.mit.edu>
2014-12-03 08:09:22 -0800

    Cleaned up a bit.

--- Lib/GrsHelper.cs
+++ Lib/GrsHelper.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lib;
 
 namespace Lib
 {
     public class GrsHelper
     {
-        public static GRS.ReleaseRights[] GetReleaseRights(string upc, string searchOption)
+        public static GRS.ReleaseRights[] GetReleaseRights(string id, string searchOption)
         {
-            var client = new Lib.GRS.ReleaseDataProviderClient();
-            var response = client.GetReleaseRights(upc, searchOption);
+            var client = new GRS.ReleaseDataProviderClient();
+            var response = client.GetReleaseRights(id, searchOption);
             client.Close();
             return response;
         }
 
         public static GRS.RepertoireRightsSearchResult[] GetReleasesSearch()
         {
-            var client = new Lib.GRS.ReleaseClient();
+            var client = new GRS.ReleaseClient();
             var criteria = new GRS.SearchRepertoireCriteria();
             var response = client.GetReleasesSearch(false, criteria);
             client.Close();
